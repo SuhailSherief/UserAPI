e6ce8dabb8716f125ad67a9f01886821d8f67b54
SuhailSherief <suhailscreed@gmail.com>
2022-10-11 23:30:27 +0530

    added api services for update and delete

--- routes/routes.js
+++ routes/routes.js
@@ -5,83 +5,83 @@ const signUpTemplateCopy = require('../models/SignUpModel')
 
 router.post('/signup', (request, response) => {
     const signedUpUser = new signUpTemplateCopy({
-        gender:request.body.gender,
+        gender: request.body.gender,
         name: {
-        title:request.body.title,
-        first:request.body.first,
-        last:request.body.last,
+            title: request.body.title,
+            first: request.body.first,
+            last: request.body.last,
         },
         location: {
-        city:request.body.city,
-        state:request.body.state,
-        country:request.body.country,
-        postcode:request.body.postcode,
+            city: request.body.city,
+            state: request.body.state,
+            country: request.body.country,
+            postcode: request.body.postcode,
         },
-        email:request.body.email,
+        email: request.body.email,
         login: {
-        username:request.body.username,
-        password:request.body.password,
+            username: request.body.username,
+            password: request.body.password,
         },
         dob: {
-            date:request.body.date,
-            age:request.body.age,
+            date: request.body.date,
+            age: request.body.age,
         },
-        phone:request.body.phone,
-        picture:request.body.picture,
+        phone: request.body.phone,
+        picture: request.body.picture,
 
     })
     signedUpUser.save()
-    .then(data => {
-        response.json(data)
-    })
-    .catch(error => {
-        response.json(error)
-    } )
+        .then(data => {
+            response.json(data)
+        })
+        .catch(error => {
+            response.json(error)
+        })
 })
 
 router.post('/search', async (request, response) => {
     let type = request.body.type.trim();
     let search = request.body.payload.trim();
-    //console.log(type, search)
-    if(type == "name")
-    {
-        let name = await signUpTemplateCopy.find({"name.first": search}).exec();
-        response.send({search: name})
+    if (type == "name") {
+        let users = await signUpTemplateCopy.find({ "name.first": {$regex: new RegExp('^'+search+'.*', 'i')} }).exec();
+        response.send({ result: users })
     }
-    if(type == "email")
-    {
-        let email = await signUpTemplateCopy.find({email: search}).exec();
-        response.send({search: email})
+    if (type == "email") {
+        let users = await signUpTemplateCopy.find({ email: {$regex: new RegExp('^'+search+'.*', 'i')} }).exec();
+        response.send({ result: users })
     }
-    if(type == "phone")
-    {
-        let phone = await signUpTemplateCopy.find({phone: search}).exec();
-        response.send({search: phone})
+    if (type == "phone") {
+        let users = await signUpTemplateCopy.find({ phone: {$regex: new RegExp('^'+search+'.*', 'i')} }).exec();
+        response.send({ result: users })
     }
-    if(type == "gender")
-    {
-        let gender = await signUpTemplateCopy.find({gender: search}).exec();
-        response.send({search: gender})
+    if (type == "gender") {
+        let users = await signUpTemplateCopy.find({ gender: {$regex: new RegExp('^'+search+'.*', 'i')} }).exec();
+        response.send({ result: users })
     }
-    if(type == "location")
-    {
-        let postcode = await signUpTemplateCopy.find({"location.postcode": search}).exec();
-        response.send({search: postcode})
+    if (type == "location") {
+        let users = await signUpTemplateCopy.find({ "location.postcode": {$regex: new RegExp('^'+search+'.*', 'i')} }).exec();
+        response.send({ result: users })
     }
-    if(type == "address")
-    {
-        let city = await signUpTemplateCopy.find({"location.city": search}).exec();
-        response.send({search: city})
+    if (type == "address") {
+        let users = await signUpTemplateCopy.find({ "location.city": {$regex: new RegExp('^'+search+'.*', 'i')} }).exec();
+        response.send({ result: users })
     }
-    if(type == "dob")
-    {
-        let date = await signUpTemplateCopy.find({"dob.date": search}).exec();
-        response.send({search: date})
+    if (type == "dob") {
+        let users = await signUpTemplateCopy.find({ "dob.date": {$regex: new RegExp('^'+search+'.*', 'i')} }).exec();
+        response.send({ result: users })
     }
 })
 
-// router.put('/update', (request, response) => {
+router.patch('/update/:id', async (request, response) => {
+    let id = request.params.id;
+    let updatedData = request.body;
+    signUpTemplateCopy.findByIdAndUpdate(id, { $set: updatedData }).exec();
+    response.send({ status: "Success" })
+})
 
-// })
-
+router.delete('/delete/:id', async (request, response) => {
+    const user = await signUpTemplateCopy.findById(request.params.id);
+    await user.remove();
+    response.send({ status: "Success" })
+})
 module.exports = router
